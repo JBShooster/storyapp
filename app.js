f1eb06a7a65384e655051e199d7a72d3a382a76d
Joel Shooster <JBShooster@gmail.com>
2016-03-07 15:53:15 -0800

    Fixing port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 //Set Port
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || process.env.IP;
 
 // connect to our mongoDB database
 // (uncomment after you enter in your own credentials in config/db.js)
